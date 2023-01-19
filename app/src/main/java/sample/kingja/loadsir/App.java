a0dd2750e4ee22916b18d46ea3b34ed53112bd0d
shixingxing <shixingxing@yungengxin.com>
2023-01-19 10:10:05 +0800

    lib更新，迁移到jitpack

--- app/src/main/java/sample/kingja/loadsir/App.java
+++ app/src/main/java/sample/kingja/loadsir/App.java
@@ -3,7 +3,6 @@ package sample.kingja.loadsir;
 import android.app.Application;
 
 import com.kingja.loadsir.core.LoadSir;
-import com.squareup.leakcanary.LeakCanary;
 
 import sample.kingja.loadsir.callback.CustomCallback;
 import sample.kingja.loadsir.callback.EmptyCallback;
@@ -22,9 +21,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (setupLeakCanary()) {
-            return;
-        }
 
         LoadSir.beginBuilder()
                 .addCallback(new ErrorCallback())
@@ -36,13 +32,4 @@ public class App extends Application {
                 .commit();
     }
 
-    private boolean setupLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return true;
-        }
-        LeakCanary.install(this);
-        return false;
-    }
 }
